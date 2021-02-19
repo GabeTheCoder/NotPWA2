f0621ac91bbf81f7f3e02001c353858c60a1c748
Gabe The Coder <gabethecoder@icloud.com>
2021-02-19 14:40:53 -0500

    jk

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "NotPWA2",
-                      url: "https://components.geekyinteractive.com/NotPWA2/NotPWA2-1.0.2.xcframework.zip",
-                      checksum: "337a6199744a048f62cea2a312ef65277168c6158fc40a735fb04da0052c3c1c")
+                      url: "https://components.geekyinteractive.com/NotPWA2/NotPWA2-1.0.3.xcframework.zip",
+                      checksum: "39e14fa34a5081181935755dd1dd416276e95ba51e98109222c9a34fdc3a3e38")
     ]
 )
