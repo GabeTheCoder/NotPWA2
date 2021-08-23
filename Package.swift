b40709d8d12e4f7523c8c07d28d2ffde7ab6410b
Gabe The Coder <gabethecoder@icloud.com>
2021-08-23 13:39:08 -0700

    fuck u checksums

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
     targets: [
         .binaryTarget(name: "NotPWA2",
                       url: "https://components.geekyinteractive.com/NotPWA2/NotPWA2-1.1.3.xcframework.zip",
-                      checksum: "0244731371d6c929b9e5c7a51476ec9b5640b11ef65c9a8bb991f75b6ff6a154")
+                      checksum: "c4facdc68baef01b4d2bc2173d851fd42f4ef43da9eea7edb21fe9387fab2913")
     ]
 )
